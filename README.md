1fa8ac417d549f1642f7ee8d6881f18b31b0f82e
洪加煌 <395455856@qq.com>
2016-02-11 15:40:38 +0800

    爬取segmentfault文章的标题和链接

---
+++ README.md
@@ -0,0 +1,3 @@
+# webCrawler
+
+爬虫
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var proxy = require('../proxylist.js');
+var url = 'https://segmentfault.com/blogs';
+var content = [];
+var page = 1;
+
+function getSFData(res){
+    var target = url + '?page=' + page;
+    request({'url': target, 'method': 'GET'}, function (error, response, body) {
+        console.log(target);
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(body);
+            var titleP = $('h2.title');
+            var titleLength = titleP.length;
+            if(titleLength == 0){
+                return res.json(content);
+            }
+            for (var i = 0; i < titleLength; i++) {
+                var title = titleP[i];
+                var a = $(title).find('a');
+                var c = {};
+                c.title = a.text();
+                c.url = a.attr('href');
+                content.push(c);
+            }
+            page++;
+            getSFData(res);
+        }
+    })    
+} 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index',{ title: '爬虫'})
+});
+/**
+ * 爬取segmentfault的所有文章标题以及地址
+ */
+router.get('/sfArticle', function(req, res, next) {
+    getSFData(res);
+    
 });
 
 module.exports = router;
